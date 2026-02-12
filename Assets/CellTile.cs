babd2c3589c829030439c62e1d82ed813335b50a
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-02-12 13:01:30 +0100

    Add coordinates  to CellTile
    Introduce a coordinates field (Vector2Int) to track tile grid position and initialize it to (-1,-1). Change Place to accept Vector2Int coords and store them when placing. Add SetOptions(List<Sprite>) and GetCoordinates() methods to allow updating options and retrieving position. Remove several Debug.Log debug prints from CheckCompatibility and apply minor whitespace/formatting cleanup. Note: Place signature changed (breaking API consumers).

--- Assets/CellTile.cs
+++ Assets/CellTile.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
-public class CellTile 
+public class CellTile
 {
-    public GameObject selfObject {  get; private set; }
+    public GameObject selfObject { get; private set; }
 
     private SpriteRenderer spriteRenderer;
 
@@ -13,26 +13,33 @@ public class CellTile
 
     private List<Sprite> options = new List<Sprite>();
 
+    private Vector2Int coordinates = new Vector2Int(-1, -1);
     public CellTile(GameObject cell, List<Sprite> options)
     {
         selfObject = cell;
-        
+
+        coordinates = new Vector2Int(-1, -1);
         spriteRenderer = selfObject.GetComponent<SpriteRenderer>();
         selfObject.SetActive(false);
         this.options = options;
         isPlaced = false;
     }
+    public void SetOptions(List<Sprite> options)
+    {
+        this.options = options;
+    }
     public void UpdateSprite(Sprite sprite)
     {
         spriteRenderer.sprite = sprite;
     }
-    public string[] Place()
+    public string[] Place(Vector2Int coords)
     {
         if (options.Count < 0)
         {
             Debug.LogWarning("No available options");
             return null;
-        } 
+        }
+        coordinates = coords;
         isPlaced = true;
         int rand = UnityEngine.Random.Range(0, GetOptionsCount());
         UpdateSprite(options[rand]);
@@ -45,7 +52,10 @@ public class CellTile
     {
         return options.Count;
     }
-    
+    public Vector2Int GetCoordinates()
+    {
+        return coordinates;
+    }
 
     public void CheckCompatibility(string origin, int side)
     {
@@ -55,30 +65,25 @@ public class CellTile
             string[] marks = TileData.instance.GetData(options[i]);
             if (side == 0)      //origin:left - this : right
             {
-                Debug.Log((side, origin, marks[2], ReverseString(marks[2]), origin != ReverseString(marks[2])));
                 if (origin != ReverseString(marks[2]))
                     toRemove.Add(options[i]);
             }
             else if (side == 1) //origin:up - this : down
             {
-                Debug.Log((side, origin, marks[3], ReverseString(marks[3]), origin != ReverseString(marks[3])));
                 if (origin != ReverseString(marks[3]))
                     toRemove.Add(options[i]);
             }
             else if (side == 2) //origin:right - this : left
             {
-                Debug.Log((side, origin, marks[0], ReverseString(marks[0]), origin != ReverseString(marks[0])));
                 if (origin != ReverseString(marks[0]))
                     toRemove.Add(options[i]);
             }
             else if (side == 3) //origin:down - this : up
             {
-                Debug.Log((side, origin, marks[1], ReverseString(marks[1]), origin != ReverseString(marks[1])));
                 if (origin != ReverseString(marks[1]))
                     toRemove.Add(options[i]);
             }
         }
-        Debug.Log((side, toRemove.Count));
         foreach(Sprite i in toRemove)
         {
             options.Remove(i);
