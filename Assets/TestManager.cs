0c311b265ed47706443c779cee62d0216e434200
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-03-03 00:05:16 +0100

    Add input controls and generation modes
    Introduce GenerationType enum and input-driven controls to TestManager. Adds a serialized ProceduralGenerationManager reference, handling for keyboard shortcuts (Space: init grid, G: single generation, T: multiple generations test, F: step-by-step generation, R: scene reset). Manages coroutines for single/multiple/step-by-step generation, stops existing coroutines when restarting, and implements MultipleGeneration to run N test generations while waiting for each to finish. Also adds SceneManagement and related using directives.

--- Assets/TestManager.cs
+++ Assets/TestManager.cs
@@ -1,18 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+
+enum GenerationType
+{
+    InitGridOnly,
+    StepByStepGeneration,
+    SingleGeneration,
+    MultipleGenerations
+}
 public class TestManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private ProceduralGenerationManager generationManager;
+
+    private Coroutine multipleGenerationCoroutine;
+    private Coroutine generationCoroutine;
+    private bool stepByStepMode;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))        //inits grid only
+        {
+            StartGeneration(GenerationType.InitGridOnly);
+        }
+        if (Input.GetKeyDown(KeyCode.G))            //starts single generation process
+        {
+            StartGeneration(GenerationType.SingleGeneration);
+        }
+        if (Input.GetKeyDown(KeyCode.T))            //starts N generation processes for testing purposes
+        {
+            StartGeneration(GenerationType.MultipleGenerations);
+        }
+        if (Input.GetKeyDown(KeyCode.F))            //used for step by step generation
+        {
+            StartGeneration(GenerationType.StepByStepGeneration);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))           //safety check, resets the scene
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartGeneration(GenerationType type)
     {
-        
+        if (generationManager.isRunning)
+        {
+            if (stepByStepMode && type == GenerationType.StepByStepGeneration)
+            {
+                generationManager.goNext = true;
+                return;
+            }
+
+            if (multipleGenerationCoroutine != null)
+                StopCoroutine(multipleGenerationCoroutine);
+            
+            if (generationCoroutine != null)
+                StopCoroutine(generationCoroutine);
+        }
+
+        stepByStepMode = false;
+        generationManager.isRunning = false;
+        generationManager.Setup();
+
+        switch (type)
+        {
+            case GenerationType.InitGridOnly:
+                break;
+
+            case GenerationType.StepByStepGeneration:
+                stepByStepMode = true;
+                generationCoroutine = StartCoroutine(generationManager.GenerateMapWithBacktracking(stepByStep: true));             
+                break;
+
+            case GenerationType.SingleGeneration:
+                generationCoroutine = StartCoroutine(generationManager.GenerateMapWithBacktracking());
+                break;
+
+            case GenerationType.MultipleGenerations:
+                multipleGenerationCoroutine = StartCoroutine(MultipleGeneration(20));
+                break;
+        }
     }
+
+
+
+
+    private IEnumerator MultipleGeneration(int num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            while (generationManager.isRunning)
+            {
+                yield return null;
+            }
+            generationManager.Setup();
+            generationCoroutine = StartCoroutine(generationManager.GenerateMapWithBacktracking(isTestEnv: true));
+        }
+    }
+
+
 }
